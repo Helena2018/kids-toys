35fb778fc294f893e3d27557806b5d16364c411d
Helena2018 <helena2020liu@gmail.com>
2022-08-11 11:31:15 +1200

    categories setting

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ const App = () => {
     {
       id: 1,
       title: 'Cars',
+    },
+    {
+      id: 2,
+      title: 'Dolls',
+    },
+    {
+      id: 3,
+      title: 'Aeroplanes',
+    },
+    {
+      id: 4,
+      title: 'Learning',
+    },
+    {
+      id: 5,
+      title: 'Cartoons',
     }
   ]
 
