b91e2366cd1faeeefff8b0927c3ada491c17a138
Helena2018 <helena2020liu@gmail.com>
2022-08-16 11:54:08 +1200

    nav style updating

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,8 @@ import { Fragment } from 'react'
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 
+import './navigation.style.scss';
+
 const Navigation = () => {
   return (
     <Fragment>
