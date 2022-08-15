d7ea28ece0d74e4c8ddc745c14f8c63834bc2b00
Helena2018 <helena2020liu@gmail.com>
2022-08-15 21:52:19 +1200

    import Link to shop page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,16 @@
 import { Fragment } from 'react'
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
-        <h1>The navigation bar</h1>
+        <div>Logo</div>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
       </Fragment>
