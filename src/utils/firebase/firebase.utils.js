b25cd14b12d088a6e39f036168b57bc8c4203148
Helena2018 <helena2020liu@gmail.com>
2022-08-16 14:10:09 +1200

    import initializeApp from firebase/app

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbh5Etu_O5quhVVCcRAs8vUfqWFZTGdaE",
+  authDomain: "react-shopping-clothes.firebaseapp.com",
+  projectId: "react-shopping-clothes",
+  storageBucket: "react-shopping-clothes.appspot.com",
+  messagingSenderId: "524930468755",
+  appId: "1:524930468755:web:7d4dda178a5f2b70b53666"
+};
+
+const app = initializeApp(firebaseConfig);
+
