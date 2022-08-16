8fb5d0acee088b9abe23ca876bff655d329d506f
Helena2018 <helena2020liu@gmail.com>
2022-08-16 16:46:38 +1200

    export userDocRef using async/await

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -31,3 +31,11 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+}
