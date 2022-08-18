80e3744fc4ae2ad3723a04a818cf30b74b3e2759
Helena2018 <helena2020liu@gmail.com>
2022-08-18 16:52:59 +1200

    reset form field

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,6 @@
-import { async } from '@firebase/util';
 import { useState } from 'react';
 
-import  createAuthUserWithEmailAndPassword from '../../utils/firebase/firebase.utils';
+import  { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
   displayName: '',
@@ -14,9 +13,31 @@ const SignUpForm = () => {
   const [ formFields, setFormFields ] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if ()
+
+    if (password !== confirmPassword) {
+      alert("passwords do not match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(email, password);
+      
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+
+    } catch(error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Cannot create user, email already in use');
+      } else {
+        console.log('user creation encountered an error', error);
+      }
+    }
   }
 
   const handleChange = (event) => {
@@ -28,7 +49,7 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onClick={() => {}}>
+      <form onClick={handleSubmit}>
         <label>Display Name</label>
         <input 
           type='text' 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,6 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-  signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import {
@@ -37,7 +36,7 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
   if (!userAuth) return;
   
   const userDocRef = doc(db, 'users', userAuth.uid);
@@ -52,7 +51,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
       await setDoc(userDocRef, {
         displayName,
         email,
-        createdAt
+        createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log('error creating the user', error.message)
