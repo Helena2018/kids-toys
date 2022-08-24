846a50faa3126d6280ca7432e895936d6ee30216
Helena2018 <helena2020liu@gmail.com>
2022-08-24 19:42:44 +1200

    import cart context to cart icon compoent to realiz toggle func

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,8 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 import './cart-icon.style.scss'
 
 const CartIcon = () => {
