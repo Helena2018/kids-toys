9b49d7287ebadb16459421af501259312f8de3a3
Helena2018 <helena2020liu@gmail.com>
2022-08-24 19:39:04 +1200

    import is cart open to navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment, useContext } from 'react'
 
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import { Outlet, Link } from 'react-router-dom';
@@ -13,6 +15,7 @@ import './navigation.style.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -33,7 +36,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        <CartDropdown />
+       {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
       </Fragment>
