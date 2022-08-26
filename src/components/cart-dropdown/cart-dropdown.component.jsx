53c7f859452e28dd4ca96cec1235625bdd7a0a36
Helena2018 <helena2020liu@gmail.com>
2022-08-26 12:43:41 +1200

    transform cartItems to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { CartContext } from '../../contexts/cart.context';
 
 import './cart-dropdown.style.scss';
@@ -9,6 +11,11 @@ import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigation = useNavigate();
+  
+  const goToCheckOut = () => {
+    navigation('/checkout')
+  }
 
   return (
     <div className='cart-dropdown-container'>
@@ -17,7 +24,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckOut}>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- src/routes/check-out/check-out.component.jsx
+++ src/routes/check-out/check-out.component.jsx
@@ -1,9 +1,25 @@
 import './check-out.style.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 const CheckOut = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div>
       <h1>I am the check out page</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 };
