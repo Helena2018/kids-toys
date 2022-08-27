99335a8a49f32018fcac3492b73c35ecbff24be0
Helena2018 <helena2020liu@gmail.com>
2022-08-27 12:18:50 +1200

    add className to check-out component

--- src/routes/check-out/check-out.component.jsx
+++ src/routes/check-out/check-out.component.jsx
@@ -7,9 +7,25 @@ const CheckOut = () => {
   const { cartItems, addItemToCart, removeItemToCart } = useContext(CartContext);
 
   return (
-    <div>
-      <h1>I am the check out page</h1>
-      <div>
+      <div className='checkout-container'>
+        <div className='checkout-header'>
+          <div className='header-block'>
+            <span>Product</span>
+          </div>
+          <div className='header-block'>
+            <span>Description</span>
+          </div>
+          <div className='header-block'>
+            <span>Quantity</span>
+          </div>
+          <div className='header-block'>
+            <span>Price</span>
+          </div>
+          <div className='header-block'>
+            <span>Remove</span>
+          </div>
+        </div>
+
         {cartItems.map((cartItem) => {
           const { id, name, quantity } = cartItem;
           return (
@@ -24,7 +40,7 @@ const CheckOut = () => {
             </div>
           )
         })}
-      </div>
+        <span className='total'>Total: 0</span>
     </div>
   );
 };
