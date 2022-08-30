ab2ba21ef2201631cedf25feeaa02609cc268e5b
Helena2018 <helena2020liu@gmail.com>
2022-08-30 12:28:58 +1200

    shop data updated

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -31,7 +31,7 @@ const CheckOut = () => {
         {cartItems.map((cartItem) => (
           <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
-        <span className='total'>Total: {cartTotal}</span>
+        <span className='total'>Total: ${cartTotal}</span>
     </div>
   );
 };
