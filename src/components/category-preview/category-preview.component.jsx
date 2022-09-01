cfba4f409ac8ec7c571928e1e893c0a7fe49db36
Helena2018 <helena2020liu@gmail.com>
2022-09-01 21:35:51 +1200

    category preview component with less than 4 products for each category

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,7 +1,20 @@
 import './category-preview.style.scss';
 
-const CategoryPreview = () => {
-
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className='category-preview-container'>
+      <h2>
+        <span className='title'>{title.toUpperCase()}</span>
+      </h2>
+      <div className='preview'>
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProdcutCard key={product.id} product={product}/>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default CategoryPreview;
