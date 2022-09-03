d888782c6d942c77245cf8e4d67b96de8e0958a4
Helena2018 <helena2020liu@gmail.com>
2022-09-03 22:45:40 +1200

    import useNavigate to direct route to each items

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,17 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from '../category/category.component';
 
-import './shop.style.scss'
-
 const Shop = () => {
   
   return (
-    <div className='shop-container'>
       <Routes>
         <Route index element={<CategoriesPreview />} />
         <Route path=':category' element={<Category />} />
       </Routes>
-    </div>
   );
 };
 
