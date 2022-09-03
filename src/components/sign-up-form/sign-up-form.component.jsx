3aa37fc41bbaf202c810c8a09074fb7e6ddb0576
Helena2018 <helena2020liu@gmail.com>
2022-09-03 22:08:03 +1200

    converted sign up form style from scss to js

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -6,7 +6,7 @@ import  {
 } from '../../utils/firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
-import './sign-up-form.style.scss';
+import { SignUpContainer } from './sign-up-form.style';
 import Button from '../button/button.component';
 
 
@@ -55,7 +55,7 @@ const SignUpForm = () => {
   }
 
   return (
-    <div className='sign-up-container'>
+    <SignUpContainer>
       <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -97,7 +97,7 @@ const SignUpForm = () => {
 
         <Button type='submit'>Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   )
 };
 
