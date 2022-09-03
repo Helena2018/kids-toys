2d47b708e3d7a6fd6b28c76876555dee984655cc
Helena2018 <helena2020liu@gmail.com>
2022-09-03 15:47:16 +1200

    import the classe of google btn type to sign in

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,18 +1,14 @@
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 import  { 
   signInWithGooglePopup, 
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
  } from '../../utils/firebase/firebase.utils';
 
-
 import './sign-in-form.style.scss';
-
-
 
 const defaultFormFields = {
   email: '',
@@ -89,7 +85,7 @@ const SignInForm = () => {
         />
         <div className='buttons-container'>
           <Button type='submit'>Sign In</Button>
-          <Button onClick={signInWithGoogle} buttonType='google' type='button'>Google Sign In</Button>
+          <Button onClick={signInWithGoogle} buttonType={BUTTON_TYPE_CLASSES.google}type='button'>Google Sign In</Button>
         </div>
       </form>
     </div>
