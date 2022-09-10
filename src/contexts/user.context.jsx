f45d4252cf518e74c3fea36dd6627d4af584d75f
Helena2018 <helena2020liu@gmail.com>
2022-09-10 14:08:37 +1200

    create rootReduce and userReduce

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -13,6 +13,10 @@ export const USER_ACTION_TYPE = {
   SET_CURRENT_USER: 'SET_CURRENT_USER',
 };
 
+const INITIAL_STATE = {
+  currentUser: null,
+}
+
 const userReducer = (state, action) => {
   const { type, payload } = action;
 
@@ -26,10 +30,6 @@ const userReducer = (state, action) => {
       throw new Error(`unhandled type ${type} in userReducer`);
   }
 };
-
-const INITIAL_STATE = {
-  currentUser: null,
-}
 
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer,INITIAL_STATE )
