05e346d9e292deb24cecdef1f0bf91a3cc67f319
Helena2018 <helena2020liu@gmail.com>
2022-09-14 20:00:59 +1200

    using persit library to setting local store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 // import logger from 'redux-logger';
 
+import { persiststore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
 import { rootReducer } from './root-reducer';
 
 const loggerMiddleware = (store) => (next) => (action) => {
@@ -9,6 +13,13 @@ const loggerMiddleware = (store) => (next) => (action) => {
   }
   next(action);
 }
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+}
+
 const middleWares = [loggerMiddleware];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
