7ffe46280f46255b01d6ac4720572a79da501706
Helena2018 <helena2020liu@gmail.com>
2022-09-15 11:46:06 +1200

    add and import thunk to store

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
+import thunk from 'redux-thunk';
 
 
  
@@ -16,7 +17,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
